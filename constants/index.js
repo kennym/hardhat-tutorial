c38ee6640a6b6bdad311e6287ba241943574d75c
Kenny Meyer <kenny@kennymeyer.net>
2022-05-15 23:32:53 -0400

    update metadata url

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xf9A52a07B8145E77108CC2B4a4a3ccFF5eee6930";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://whitelist-dapp-ecru.vercel.app/api";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
